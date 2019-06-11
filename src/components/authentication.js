f4e8f541672ceca87d26cebdc6c5155fad1f76bb
Dustin Schau <dustinschau@gmail.com>
2019-06-11 07:42:40 +0300

    chore: start branch

--- src/components/authentication.js
+++ src/components/authentication.js
@@ -1,63 +1,16 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 import getAuth from '../util/authentication'
 
 const { Consumer, Provider } = React.createContext(`authentication`)
 
-class Authentication extends Component {
-  state = {}
-  auth = undefined
+const AUTH_SERVICE = 'gmail'
 
-  async componentDidMount() {
-    this.auth = getAuth()
-
-    const authenticated = await this.auth.isLoggedIn('gmail')
-
-    this.setState({
-      authenticated,
-    })
-  }
-
-  login = (service = 'gmail') => {
-    return async () => {
-      await this.auth.login(service)
-
-      const authenticated = await this.auth.isLoggedIn(service)
-
-      this.setState({
-        authenticated,
-      })
-    }
-  }
-
-  logout = (service = 'gmail') => {
-    return async () => {
-      await this.auth.logout(service)
-
-      window.localStorage.clear()
-
-      this.setState({
-        authenticated: false,
-      })
-    }
-  }
-
-  render() {
-    return (
-      <Provider
-        value={{
-          ...this.state,
-          login: this.login,
-          logout: this.logout,
-        }}
-      >
-        {this.props.children}
-      </Provider>
-    )
-  }
+function Authentication({ children }) {
+  return children
 }
 
-Authentication.Consumer = Consumer
 Authentication.Provider = Authentication
+Authentication.Consumer = Consumer
 
 export default Authentication
