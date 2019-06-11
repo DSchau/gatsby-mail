bef7749fdb75c87a83afa811d8b140e89c8be2cc
Dustin Schau <dustinschau@gmail.com>
2019-06-11 07:24:16 +0300

    chore: bump deps

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,7 +23,7 @@ const BottomRight = styled.div({
 
 const IndexPage = () => (
   <Query
-    pollInterval={5000}
+    pollInterval={1000}
     query={gql`
       query GetMessages {
         google {
